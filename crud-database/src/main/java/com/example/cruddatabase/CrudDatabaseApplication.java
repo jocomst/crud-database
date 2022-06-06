e9334eedf2c61e56dbb199788bc0288c7a36f3db
John <johncomstock9716@gmail.com>
2022-06-06 16:14:45 -0400

    Initial commit

---
+++ crud-database/src/main/java/com/example/cruddatabase/CrudDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruddatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDatabaseApplication.class, args);
+	}
+
+}
